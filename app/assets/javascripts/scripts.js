4bc829b8840bd4cb37c4efa6adb1a5b12fc86777
Hoier <yurimathe.yp@gmail.com>
2014-03-21 13:53:09 -0300

    Funciona, caralho

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,44 +1,11 @@
-<script type="text/javascript" src="jquery-1.3.2.min.js"></script>
-    <script type="text/javascript">
-    $(document).ready(function() {
-        line(50,30, 100, 150);
-        circle(110,110, 100);
-    });
+= require jquery
+= require jquery_ujs
+= require turbolinks
+= require_tree .
 
-    function line(x1, y1, x2, y2) {
-        c = $(document.body);
-        var dx = Math.abs(x2-x1);
-        var dy = Math.abs(y2-y1);
-        var d = Math.max(dx, dy);
-        var i=0;
-        for(i=0; i < d; i++) {
-            var img = $(document.createElement('img')).attr('src', 'blank.gif');
-            var div = $(document.createElement('div')).width(1).height(1).css({'background-color': '#f00', position: 'absolute', left: Math.min(x1,x2)+(i*dx/d), top: Math.min(y1,y2)+(i*dy/d) });
-            div.append(img);
-            c.append(div);
-        }
-    }
-    function circle(x, y, r) {
-        c = $(document.body);
-        var l = 2 * Math.PI * r;
-        var i=0;
-        for(i=0; i < l * (1+((10-Math.log(r+1))/10)); i++) {
-            var x2 = r * Math.sin(360 * i/l);
-            var y2 = r * Math.cos(360 * i/l);
-            var img = $(document.createElement('img')).attr('src', 'blank.gif');
-            var div = $(document.createElement('div')).width(1).height(1).css({'background-color': '#f00', position: 'absolute', left: x+x2, top: y+y2 });
-            div.append(img);
-            c.append(div);
-        }
-    }
-    </script>
-    <style type="text/css">
-        <!--
-        .dragme{position:relative;}
-        -->
-    </style>
-    
-    <script type="text/javascript">
+  $(function() {
+    $( "#draggable" ).draggable();
+  });
         <!-- This script and many more are available free online at -->
         <!-- Created by: elouai.com -->
         <!-- Início
@@ -79,10 +46,7 @@
         document.onmousedown=selectmouse;
         document.onmouseup=new Function("isdrag=false");
         // Fim -->
-         
-    </script>
 
-    <script>
     $(document).ready(function() {
 
      $("#example div").click(function() {
@@ -91,7 +55,8 @@
      });
    
      });
-    </script>
 
-    <script>(function(){ var c = document.getElementById(''); var ctx = c.getContext("2d"); /* on mouse "mexe" */ });</script>
-
+    (function(){ 
+      var c = document.getElementById(''); 
+      var ctx = c.getContext("2d"); /* on mouse "mexe" */ 
+    });
