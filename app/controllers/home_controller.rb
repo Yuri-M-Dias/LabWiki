3258de393993c9c9a7502ea9be8daf9762713767
Hoier <yurimathe.yp@gmail.com>
2014-07-07 17:56:43 -0300

    Consertando modal e arrumando alguns arquivos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,4 @@ class HomeController < ApplicationController
   	@nodes = Node.all
   	@neighs = Neighborhood.all
   end
-
-  def scriptresult
-  	if !current_user
-  		redirect_to sign_in_path
-  	end
-  end
-
 end
--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -2,7 +2,7 @@ module LayoutHelper
 
   def bootstrap_version
     # Choose the Bootstrap version you would like to use from the CDN
-    "3.1.1"
+    "3.2.0"
   end
 
 end
